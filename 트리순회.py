a0744810231dac021d6c7157627425187f22287f
seungkey <84698380+firstep710@users.noreply.github.com>
2021-06-05 21:12:46 +0900

    (1991번)트리순회
    재귀함수 연습문제

---
+++ 트리순회.py
@@ -0,0 +1,26 @@
+def preorder(root):
+    if root != ".":
+        print(root,end='')
+        preorder(tree[root][0])
+        preorder(tree[root][1])
+
+def inorder(root):
+    if root != ".":
+        inorder(tree[root][0])
+        print(root,end='')
+        inorder(tree[root][1])
+
+def postorder(root):
+    if root != ".":
+        postorder(tree[root][0])
+        postorder(tree[root][1])
+        print(root,end='')
+tree={}
+for i in range(int(input())):
+    root, left, right=input().split()
+    tree[root]=[left,right]
+preorder('A')
+print()
+inorder('A')
+print()
+postorder('A')
